70c9f3f67caf64217d4da113172f5ced3ecdb939
stepan <stepanstepanov@gmail.com>
2025-05-01 16:41:10 +0200

    add more DB indexes

--- migrations/Version20250428142936.php
+++ migrations/Version20250428142936.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20250428142936 extends AbstractMigration
 {
     public function getDescription(): string
@@ -28,7 +25,9 @@ final class Version20250428142936 extends AbstractMigration
                 request TEXT NULL,
                 httpStatusCode ENUM('100','101','102','103','200','201','202','203','204','205','206','207','208','226','300','301','302','303','304','305','307','308','400','401','402','403','404','405','406','407','408','409','410','411','412','413','414','415','416','417','418','421','422','423','424','425','426','428','429','431','451','500','501','502','503','504','505','506','507','508','510','511') NULL,
                 logLine TEXT NOT NULL,
-                INDEX idx_dateTime (dateTime)
+                INDEX idx_dateTime (dateTime),
+                INDEX idx_serviceName (serviceName),
+                INDEX idx_httpStatusCode (httpStatusCode)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 SQL);
     }
