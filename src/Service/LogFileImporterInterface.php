4a9e6eb91d26404be2b131034bdf067c978d2b33
stepan <stepanstepanov@gmail.com>
2025-05-03 17:25:11 +0200

    declare exception

--- src/Service/LogFileImporterInterface.php
+++ src/Service/LogFileImporterInterface.php
@@ -3,8 +3,12 @@
 namespace App\Service;
 
 use App\DTO\ImportResult;
+use App\Exception\CannotOpenFileException;
 
 interface LogFileImporterInterface
 {
+    /**
+     * @throws CannotOpenFileException
+     */
     public function importFile(string $filePath): ImportResult;
 }
